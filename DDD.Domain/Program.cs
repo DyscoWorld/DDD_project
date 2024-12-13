70b0a4a1b477f193569a2df48c0702849d737c54
smasher <iluha_m_74@mail.ru>
2024-12-13 20:37:10 +0500

    Project initialization

---
+++ DDD.Domain/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DDD.Domain
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
