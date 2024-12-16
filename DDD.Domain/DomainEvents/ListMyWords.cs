1c2de4a1c98cd7165a82186333cae8217c317bdf
EgorVastikov <140184620+EgorVastikov@users.noreply.github.com>
2024-12-17 01:55:24 +0500

    Задача 3 сделана. Добавил классы событий и диспетчер событий (#7)
    * Задача 3 сделана. Добавил классы событий и диспетчер событий
    
    * Задача 3 сделана. Добавил классы событий и диспетчер событий
    
    * Добавил Service.cs и изменил DDD.Presentation/Program.cs
    
    * update
    
    * удалил DEClasses.cs

---
+++ DDD.Domain/DomainEvents/ListMyWords.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDD.Domain.DomainEvents
+{
+    // Список слов пользователя
+    public class ListMyWords : IDomainEvent
+    {
+        public void Handle()
+        {
+            // Пустая реализация
+        }
+    }
+}
